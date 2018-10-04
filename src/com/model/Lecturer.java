b9dbb54618155e4c490c8474e7fd39836dd3b045
michel <michelseth4@gmail.com>
2018-10-04 15:00:10 +0200

    fast commit

---
+++ src/com/model/Lecturer.java
@@ -0,0 +1,32 @@
+package com.model;
+
+
+public class Lecturer {
+	private int ID;
+	private String firstName;
+	private String lastName;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int id) {
+		ID = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
